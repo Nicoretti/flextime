23375b34beeea09068d352e76e41c86a4a98a9c4
Nicola Coretti <nico.coretti@gmail.com>
2022-03-06 18:33:00 +0100

    Rename variables according to go naming schema

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	var command_line cli.Cli
+	var commandLine cli.Cli
 	var now = time.Now()
 	var _, weekNumber = time.Now().ISOWeek()
-	var ctx = kong.Parse(&command_line,
+	var ctx = kong.Parse(&commandLine,
 		kong.Vars{
 			"today": time.Now().Format("2.1.2006"),
 			"day":   strconv.FormatInt(int64(time.Now().Day()), 10),
